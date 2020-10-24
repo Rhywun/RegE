bac127fb53959ac1094aaaa1a363df1444f88084
Patrick Huffer <huffer@rhywun.com>
2020-10-24 12:38:45 -0400

    Initial Commit

---
+++ RegE/RegEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegEApp.swift
+//  RegE
+//
+//  Created by Patrick Huffer on 2020-10-24.
+//
+
+import SwiftUI
+
+@main
+struct RegEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
